4b2813477ecabb943bcc5b296f6e61ad23685c12
Katleho06Molefe <Hulang606@gmail.com>
2025-06-19 15:05:02 +0100

    MongoDB Connection
    The Connection of MongoDB

--- server/server.js
+++ server/server.js
@@ -4,22 +4,34 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Test route
 app.get('/', (req, res) => {
-  res.send('Hello from backend!');
+  res.send(' Hello from backend!');
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+if (!MONGO_URI) {
+  console.error('MONGO_URI not defined in .env file');
+  process.exit(1);
+}
+
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => {
-  console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  console.log(' MongoDB connected');
+  app.listen(PORT, () => {
+    console.log(` Server running at http://localhost:${PORT}`);
+  });
 })
-.catch(err => console.log(err));
+.catch((error) => {
+  console.error(' MongoDB connection error:', error);
+});
