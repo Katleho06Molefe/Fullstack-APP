6dfdd26605dca441a32fb48b4ec176c8d4cf1f69
Katleho06Molefe <Hulang606@gmail.com>
2025-06-22 00:40:59 +0100

    App.js
    App.js update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>JWT Login Page</h1>
-      <Login />
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
